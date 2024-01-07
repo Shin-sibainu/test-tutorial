6c875cf64010552e6813f769155e4f4e214bb64f
Shin-sibainu <shincode0712@gmail.com>
2024-01-07 11:09:36 +0900

    mod

--- src/components/greet/greet.spec.tsx
+++ src/components/greet/greet.spec.tsx
@@ -2,14 +2,14 @@ import { render, screen } from "@testing-library/react";
 import Greet from "./greet";
 
 describe("Greet", () => {
-  test("renders correctly", () => {
+  it("renders correctly", () => {
     render(<Greet />);
     const textElement = screen.getByText(/hello/i);
     expect(textElement).toBeInTheDocument();
   });
 
   describe("Nested", () => {
-    test("renders with a name", () => {
+    it("renders with a name", () => {
       render(<Greet name="ShinCode" />);
       const textElement = screen.getByText("Hello ShinCode");
       expect(textElement).toBeInTheDocument();
