d420b82e574e31999ecbeeade44cd438dcef5e5c
Shin-sibainu <shincode0712@gmail.com>
2024-01-16 10:41:32 +0900

    mod

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -30,4 +30,39 @@ describe("Counter", () => {
     const counterElement = screen.getByRole("heading");
     expect(counterElement).toHaveTextContent("1");
   });
+
+  test("renders a count of 10 after clicking the set button", async () => {
+    const user = userEvent.setup();
+    render(<Counter />);
+    const amountInput = screen.getByRole("spinbutton");
+    await user.type(amountInput, "10");
+    expect(amountInput).toHaveValue(10);
+    const setButton = screen.getByRole("button", {
+      name: "Set",
+    });
+    await user.click(setButton);
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("10");
+  });
+
+  test("elements are focused in the right order", async () => {
+    const user = userEvent.setup();
+    render(<Counter />);
+    const amountInput = screen.getByRole("spinbutton");
+    const setButton = screen.getByRole("button", {
+      name: "Set",
+    });
+    const incrementButton = screen.getByRole("button", {
+      name: "Increment",
+    });
+
+    await user.tab();
+    expect(incrementButton).toHaveFocus();
+
+    await user.tab();
+    expect(amountInput).toHaveFocus();
+
+    await user.tab();
+    expect(setButton).toHaveFocus();
+  });
 });
